44d6cb1e09f878486059091f2d6dce38e0fca119
koojiny <msj09252@gmail.com>
2025-09-15 23:40:34 +0900

    Initial commit

---
+++ src/main/java/com/manwon/happiness/ManwonHappinessBackendApplication.java
@@ -0,0 +1,13 @@
+package com.manwon.happiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManwonHappinessBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManwonHappinessBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manwon/happiness/ManwonHappinessBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manwon.happiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManwonHappinessBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
